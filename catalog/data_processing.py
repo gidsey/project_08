50c39b2980094c5de94b0ded57272c63065e46d7
chrisguy <chris@chrisguy.co>
2019-08-02 18:14:34 +0100

    css additions

--- catalog/data_processing.py
+++ catalog/data_processing.py
@@ -23,21 +23,21 @@ def get_popular():
     """Return the fields listed in order of most used."""
 
     ordered_fields = [('category', len(Mineral.objects.exclude(category=''))),
-                    ('formula', len(Mineral.objects.exclude(formula=''))),
-                    ('strunz_classification', len(Mineral.objects.exclude(strunz_classification=''))),
-                    ('color', len(Mineral.objects.exclude(color=''))),
-                    ('crystal_system', len(Mineral.objects.exclude(crystal_system=''))),
-                    ('unit_cell', len(Mineral.objects.exclude(unit_cell=''))),
-                    ('crystal_symmetry', len(Mineral.objects.exclude(crystal_symmetry=''))),
-                    ('cleavage', len(Mineral.objects.exclude(cleavage=''))),
-                    ('mohs_scale_hardness', len(Mineral.objects.exclude(mohs_scale_hardness=''))),
-                    ('luster', len(Mineral.objects.exclude(luster=''))),
-                    ('streak', len(Mineral.objects.exclude(streak=''))),
-                    ('diaphaneity', len(Mineral.objects.exclude(diaphaneity=''))),
-                    ('optical_properties', len(Mineral.objects.exclude(optical_properties=''))),
-                    ('refractive_index', len(Mineral.objects.exclude(refractive_index=''))),
-                    ('crystal_habit', len(Mineral.objects.exclude(crystal_habit=''))),
-                    ('specific_gravity', len(Mineral.objects.exclude(specific_gravity=''))),
-                    ('group', len(Mineral.objects.exclude(group='')))]
+                      ('formula', len(Mineral.objects.exclude(formula=''))),
+                      ('strunz_classification', len(Mineral.objects.exclude(strunz_classification=''))),
+                      ('color', len(Mineral.objects.exclude(color=''))),
+                      ('crystal_system', len(Mineral.objects.exclude(crystal_system=''))),
+                      ('unit_cell', len(Mineral.objects.exclude(unit_cell=''))),
+                      ('crystal_symmetry', len(Mineral.objects.exclude(crystal_symmetry=''))),
+                      ('cleavage', len(Mineral.objects.exclude(cleavage=''))),
+                      ('mohs_scale_hardness', len(Mineral.objects.exclude(mohs_scale_hardness=''))),
+                      ('luster', len(Mineral.objects.exclude(luster=''))),
+                      ('streak', len(Mineral.objects.exclude(streak=''))),
+                      ('diaphaneity', len(Mineral.objects.exclude(diaphaneity=''))),
+                      ('optical_properties', len(Mineral.objects.exclude(optical_properties=''))),
+                      ('refractive_index', len(Mineral.objects.exclude(refractive_index=''))),
+                      ('crystal_habit', len(Mineral.objects.exclude(crystal_habit=''))),
+                      ('specific_gravity', len(Mineral.objects.exclude(specific_gravity=''))),
+                      ('group', len(Mineral.objects.exclude(group='')))]
     ordered_fields.sort(key=itemgetter(1), reverse=True)  # Sort the list
     return ordered_fields
