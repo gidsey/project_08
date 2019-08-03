42dbbfff17ce2244873269a28717266f615c4698
chrisguy <chris@chrisguy.co>
2019-08-03 13:16:41 +0100

    import messaging improved

--- catalog/templatetags/catalog_extras.py
+++ catalog/templatetags/catalog_extras.py
@@ -7,10 +7,18 @@ from ..models import Mineral
 
 register = template.Library()
 
+# @register.simple_tag
+# def subtraction(num1, num2):
+#     """Subtract an Return the result"""
+#     return num1 - num2
+
 @register.simple_tag
-def subtraction(num1, num2):
-    """Subtract an Return the result"""
-    return num1 - num2
+def import_report(minerals_json_count, duplicate_count):
+    if minerals_json_count == duplicate_count:
+        return 'No new records found.'
+    else:
+        return "Records added successfully (" + str(duplicate_count) + " duplicates ignored)."
+
 
 @register.filter('random_mineral')
 def random_mineral(x):
@@ -20,6 +28,7 @@ def random_mineral(x):
 
 
 @register.filter('underscore_to_space')
-def underscore_to_space(input):
+def underscore_to_space(machine_name):
     """Removes underscore and replace with a SPACE"""
-    return input.replace('_', ' ')
+    return machine_name.replace('_', ' ')
+
